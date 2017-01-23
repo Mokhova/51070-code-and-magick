758b06a29ba86a64b1d02ceb14530f881ec284a7
mokhova <mokhova.ekaterina@gmail.com>
2017-01-23 22:08:57 +0500

    Вынесла нахождение максимума в отдельную функцию

--- js/stat.js
+++ js/stat.js
@@ -1,23 +1,23 @@
 'use strict';
 
- // ОБЛАКО
+ // Облако
 function drawCloud(ctx, x, y, width, height, needText) {
   var stepHor = width / 6;
   var stepVer = height / 4;
 
   ctx.beginPath();
 
-  // верх
+  // Верх
   ctx.bezierCurveTo(x, y + 10, x + stepHor, y - 20, x + stepHor * 2, y + 10);
   ctx.bezierCurveTo(x + stepHor * 2, y + 10, x + stepHor * 3, y - 30, x + stepHor * 4, y + 10);
   ctx.bezierCurveTo(x + stepHor * 4, y + 10, x + stepHor * 5, y - 20, x + width, y + 10);
 
-  // право
+  // Право
   ctx.bezierCurveTo(x + width, y + 10, x + width + 100, y + stepVer * 2, x + width, y + height - 10);
   // ctx.bezierCurveTo(x + width, y + 10, x + width + 50, y + stepVer , x + width + 30, y + stepVer * 2);
   // ctx.bezierCurveTo(x + width + 30, y + stepVer * 2, x + width + 50, y + stepVer * 3, x + width, y + height - 10);
 
-  // низ
+  // Низ
   ctx.bezierCurveTo(x + width, y + height - 10, x + stepHor * 5, y + 35 + height, x + stepHor * 4, y + height - 5);
   ctx.bezierCurveTo(x + stepHor * 4, y + height, x + stepHor * 3, y + 35 + height, x + stepHor * 2, y + height - 5);
   ctx.bezierCurveTo(x + stepHor * 2, y + height - 5, x + stepHor, y + 35 + height, x, y + height - 10);
@@ -36,7 +36,7 @@ function drawCloud(ctx, x, y, width, height, needText) {
   }
 }
 
-// РАНДОМНАЯ ЯРКОСТЬ
+// Рандомная яркость
 function randomBrightness() {
   var brightness = Math.random().toFixed(1);
   if (brightness <= 0.1) {
@@ -46,18 +46,28 @@ function randomBrightness() {
   return 'rgba(' + rgba.join(', ') + ')';
 }
 
-// КРАСИТ СТАТИСТИКУ ИГРАЮЩЕГО
+// Красит статистике играющего в красный
 function whatColor(iName) {
-  if (iName == 'Вы') {
-    return 'rgba(242, 53, 87, 1)';
+  if (iName === 'Вы') {
+    return 'rgba(242, 52, 0, 1)';
   } else {
     return randomBrightness();
   }
 }
 
+// Нахождение максимума
+function findMaximun(times){
+  var maxTime = times[0];
+    for (var i = 1; i < times.length; i++) {
+      if (maxTime < times[i]) {
+        maxTime = times[i];
+      }
+    }
+    return maxTime;
+}
+
 window.renderStatistics = function (ctx, names, times) {
-  var x = 100, y = 10;
-  var gap = 90, width = 420, height = 270, statHeight = 140;  // Вычла 10 из высоты, чтобы шрифты не прилипали
+  var x = 100, y = 10, width = 420, height = 270, gap = 90, statHeight = 140;  // Вычла 10 из высоты, чтобы шрифты не прилипали
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   drawCloud(ctx, x + 45, y + 5, width, height, 0);
@@ -65,15 +75,7 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = '#fff';
   drawCloud(ctx, x + 40, y, width, height, 1);
 
-
-  var maxTime = times[0];
-  for (var i = 1; i < times.length; i++) {
-    if (maxTime < times[i]) {
-      maxTime = times[i];
-    }
-  }
-
-  var stepForGraph = statHeight / maxTime;
+  var stepForGraph = statHeight / findMaximun(times);
 
   for (var i = 0; i < times.length; i++) {
     var iTime = Math.round(times[i]);
@@ -92,4 +94,4 @@ window.renderStatistics = function (ctx, names, times) {
 };
 
 var canvas = document.querySelector('canvas');
-window.renderStatistics(canvas.getContext('2d'), NAMES, times);
+window.renderStatistics(canvas.getContext('2d'), names, times);
