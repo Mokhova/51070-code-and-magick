e20b190b12637edb794f71f48e7c392cc9a2136e
mokhova <mokhova.ekaterina@gmail.com>
2017-01-23 13:18:16 +0500

    Правки по Travis

--- js/stat.js
+++ js/stat.js
@@ -1,42 +1,42 @@
 'use strict';
 
-//ОБЛАКО
+ // ОБЛАКО
 function drawCloud(ctx, x, y, width, height, needText) {
   var stepHor = width / 6;
   var stepVer = height / 4;
 
   ctx.beginPath();
 
-  //верх
+  // верх
   ctx.bezierCurveTo(x, y + 10, x + stepHor, y - 20, x + stepHor * 2, y + 10);
   ctx.bezierCurveTo(x + stepHor * 2, y + 10, x + stepHor * 3, y - 30, x + stepHor * 4, y + 10);
   ctx.bezierCurveTo(x + stepHor * 4, y + 10, x + stepHor * 5, y - 20, x + width, y + 10);
 
-  //право
+  // право
   ctx.bezierCurveTo(x + width, y + 10, x + width + 100, y + stepVer * 2, x + width, y + height - 10);
   // ctx.bezierCurveTo(x + width, y + 10, x + width + 50, y + stepVer , x + width + 30, y + stepVer * 2);
   // ctx.bezierCurveTo(x + width + 30, y + stepVer * 2, x + width + 50, y + stepVer * 3, x + width, y + height - 10);
 
-  //низ
+  // низ
   ctx.bezierCurveTo(x + width, y + height - 10, x + stepHor * 5, y + 35 + height, x + stepHor * 4, y + height - 5);
-  ctx.bezierCurveTo(x + stepHor * 4, y + height,  x + stepHor * 3, y + 35 + height, x + stepHor * 2, y + height - 5);
+  ctx.bezierCurveTo(x + stepHor * 4, y + height, x + stepHor * 3, y + 35 + height, x + stepHor * 2, y + height - 5);
   ctx.bezierCurveTo(x + stepHor * 2, y + height - 5, x + stepHor, y + 35 + height, x, y + height - 10);
 
-  //лево
+  // лево
   ctx.bezierCurveTo(x, y + height - 10, x - 100, y + stepVer * 2, x, y + 10);
 
-  //заливка
+  // заливка
   ctx.fill();
 
   if (needText) {
     ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-    ctx.font = "16px PT Mono";
+    ctx.font = '16px PT Mono';
     ctx.fillText('Ура вы победили!', x + 40, y + 32);
     ctx.fillText('Список результатов:', x + 40, y + 52);
   }
 }
 
-//РАНДОМНАЯ ЯРКОСТЬ
+// РАНДОМНАЯ ЯРКОСТЬ
 function randomBrightness() {
   var brightness = Math.random().toFixed(1);
   if (brightness <= 0.1) {
@@ -46,9 +46,9 @@ function randomBrightness() {
   return 'rgba(' + rgba.join(', ') + ')';
 }
 
-//КРАСИТ СТАТИСТИКУ ИГРАЮЩЕГО
+// КРАСИТ СТАТИСТИКУ ИГРАЮЩЕГО
 function whatColor(iName) {
-  if (iName  == 'Вы') {
+  if (iName == 'Вы') {
     return 'rgba(242, 53, 87, 1)';
   } else {
     return randomBrightness();
@@ -56,13 +56,15 @@ function whatColor(iName) {
 }
 
 window.renderStatistics = function (ctx, names, times) {
-  var x = 100, y = 10, gap = 90, width = 420, height = 270, statHeight = 140;  // Вычла 10 из высоты, чтобы шрифты не прилипали
+  var x = 100, y = 10;
+  var gap = 90, width = 420, height = 270, statHeight = 140;  // Вычла 10 из высоты, чтобы шрифты не прилипали
 
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   drawCloud(ctx, x + 45, y + 5, width, height, 0);
 
   ctx.fillStyle = '#fff';
   drawCloud(ctx, x + 40, y, width, height, 1);
+
 
   var maxTime = times[0];
   for (var i = 1; i < times.length; i++) {
