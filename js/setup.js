3edd84a8d48e68530e87279f7f7df16b29c04eff
mokhova <mokhova.ekaterina@gmail.com>
2017-02-01 18:00:42 +0500

    Убрала лишние переменные, переписала функции

--- js/setup.js
+++ js/setup.js
@@ -1,46 +1,6 @@
 'use strict';
 
-// Открыть/закрыть лайтбокс профиля
-var setup = document.querySelector('.setup');
-var setupOpen = document.querySelector('.setup-open');
-var setupClose = setup.querySelector('.setup-close');
-
-function toggleView(element, property) {
-  element.addEventListener('click', function () {
-    setup.classList.toggle(property);
-  });
-}
-toggleView(setupOpen, 'invisible');
-toggleView(setupClose, 'invisible');
-
-// Валидация поля
-var gamerName = document.querySelector('.setup-user-name');
-var submitSetup = setup.querySelector('.setup-submit');
-gamerName.required = true;
-gamerName.max = 50;
-
-function validateEmpty() {
-  if (!gamerName.validity.valid) {
-    if (gamerName.validity.valueMissing) {
-      gamerName.setCustomValidity('Назовись, о великий маг!');
-    } else {
-      gamerName.setCustomValidity('');
-    }
-  }
-}
-submitSetup.addEventListener('click', validateEmpty);
-
-// Раскраска волшебника
-function generateColor(element, property, colorsArray) {
-  element.addEventListener('click', function () {
-    var randomCounter = Math.floor(Math.random() * colorsArray.length);
-    element.style[property] = colorsArray[randomCounter];
-  });
-}
-
-// Красим пальто
-var wizard = document.querySelector('#wizard');
-var wizardCoat = wizard.querySelector('#wizard-coat');
+// Цвета волшебника
 var wizardCoatColors = [
   'rgb(101, 137, 164)',
   'rgb(241, 43, 107)',
@@ -49,10 +9,6 @@ var wizardCoatColors = [
   'rgb(215, 210, 55)',
   'rgb(0, 0, 0)'
 ];
-generateColor(wizardCoat, 'fill', wizardCoatColors);
-
-// Красим глаза
-var wizardEyes = wizard.querySelector('#wizard-eyes');
 var wizardEyesColors = [
   'black',
   'red',
@@ -60,10 +16,6 @@ var wizardEyesColors = [
   'yellow',
   'green'
 ];
-generateColor(wizardEyes, 'fill', wizardEyesColors);
-
-// Красим фаербол
-var fireball = setup.querySelector('.setup-fireball-wrap');
 var fireballColors = [
   '#ee4830',
   '#30a8ee',
@@ -71,4 +23,53 @@ var fireballColors = [
   '#e848d5',
   '#e6e848'
 ];
-generateColor(fireball, 'backgroundColor', fireballColors);
+
+// Открыть/закрыть профиль волшебника
+function toggleSetupView(property) {
+  var overlay = document.querySelector('.setup');
+  if (overlay.classList.contains(property)) {
+    overlay.classList.remove(property);
+  } else {
+    overlay.classList.add(property);
+  }
+}
+
+document.querySelector('.setup-open').addEventListener('click', function (evt) {
+  toggleSetupView('invisible');
+});
+document.querySelector('.setup-close').addEventListener('click', function (evt) {
+  toggleSetupView('invisible');
+});
+
+// Определение цвета
+function generateColor(evt, property, colorsArray) {
+  var randomCounter = Math.floor(Math.random() * colorsArray.length);
+  evt.currentTarget.style[property] = colorsArray[randomCounter];
+}
+
+document.querySelector('#wizard-coat').addEventListener('click', function (evt) {
+  generateColor(evt, 'fill', wizardCoatColors);
+});
+
+document.querySelector('#wizard-eyes').addEventListener('click', function (evt) {
+  generateColor(evt, 'fill', wizardEyesColors);
+});
+
+document.querySelector('.setup-fireball-wrap').addEventListener('click', function (evt) {
+  generateColor(evt, 'backgroundColor', fireballColors);
+});
+
+// Валидация поля
+var gamerName = document.querySelector('.setup-user-name');
+gamerName.required = true;
+gamerName.max = 50;
+
+function validateEmpty() {
+  if (gamerName.validity.valueMissing) {
+    gamerName.setCustomValidity('Назовись, о великий маг!');
+  } else {
+    gamerName.setCustomValidity('');
+  }
+}
+
+document.querySelector('.setup-submit').addEventListener('click', validateEmpty);
